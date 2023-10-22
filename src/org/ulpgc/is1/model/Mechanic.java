1086d1f5fa9a881628154ed219915f24bdc08b07
Victoria <vicsanfer04@gmail.com>
2023-10-22 22:49:10 +0100

    clase Mechanic

--- src/org/ulpgc/is1/model/Mechanic.java
+++ src/org/ulpgc/is1/model/Mechanic.java
@@ -1,4 +1,32 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+//atributos
 public class Mechanic {
+
+    private String name;
+    private String surname;
+    //constructor
+    public Mechanic(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
+
