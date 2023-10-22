6853868eca25999d313e4f5cf7435a9200ce9138
Victoria <vicsanfer04@gmail.com>
2023-10-22 22:50:40 +0100

    clase Phone

--- src/org/ulpgc/is1/model/Phone.java
+++ src/org/ulpgc/is1/model/Phone.java
@@ -1,4 +1,32 @@
 package org.ulpgc.is1.model;
-
+//atributo
 public class Phone {
+    private String number;
+    //cocnstructor
+    public Phone(String number) {
+        this.number = number;
+    }
+    //getter
+    public String getNumber() {
+        return number;
+    }
+    //setter
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    //relación
+    public boolean isValid() {
+        if (number == null || number.length() != 9) {
+            return false;
+        }
+
+        for (char digit : number.toCharArray()) {
+            if (!Character.isDigit(digit)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
