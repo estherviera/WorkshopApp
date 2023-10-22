a110b4408703141cf1c1fa5217bd0f1df60ecd5e
Victoria <vicsanfer04@gmail.com>
2023-10-22 22:50:16 +0100

    clase Payment

--- src/org/ulpgc/is1/model/Payment.java
+++ src/org/ulpgc/is1/model/Payment.java
@@ -1,4 +1,33 @@
 package org.ulpgc.is1.model;
 
+import java.util.Date;
+//atributos
 public class Payment {
+
+    private Date date;
+    private int amount;
+
+    //constructor
+    public Payment(Date date, int amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    //getter
+    public Date getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    //setter
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
