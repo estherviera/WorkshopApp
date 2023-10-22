b94691f87bf42a715d0ce1e67a2ebebce4758dd1
Victoria <vicsanfer04@gmail.com>
2023-10-22 20:28:26 +0100

    clase customer

--- src/org/ulpgc/is1/model/Customer.java
+++ src/org/ulpgc/is1/model/Customer.java
@@ -1,4 +1,33 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+//atributos
 public class Customer {
+    private String name;
+    private Phone phone;
+
+    //constructor
+    public Customer(String name, Phone phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
 }
+
