2483606c671b5668a2c0f923a2e03276fe5525f7
esthe <esthervieriv@gmail.com>
2023-10-22 19:37:56 +0100

    clase Plate

--- src/org/ulpgc/is1/model/Plate.java
+++ src/org/ulpgc/is1/model/Plate.java
@@ -1,4 +1,36 @@
 package org.ulpgc.is1.model;
 
 public class Plate {
+
+    //atributos
+    private String number;
+
+    //constructor
+    public Plate(String number) {
+        this.number = number;
+    }
+
+    //getter y setter
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    // relación
+    public boolean isValid() {
+        if (number == null || number.isEmpty()) {
+            return false;
+        }
+
+        for (char character : number.toCharArray()) {
+            if (!Character.isLetterOrDigit(character)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
