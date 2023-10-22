dd90711efe0007c9913152297c4500b6c5b5ccbd
Victoria <vicsanfer04@gmail.com>
2023-10-22 19:53:21 +0100

    clase SparePart

--- src/org/ulpgc/is1/model/SparePart.java
+++ src/org/ulpgc/is1/model/SparePart.java
@@ -1,4 +1,34 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+//atributos
 public class SparePart {
+
+    private String name;
+    private Integer price;
+
+    //constructor
+    public SparePart(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
 }
