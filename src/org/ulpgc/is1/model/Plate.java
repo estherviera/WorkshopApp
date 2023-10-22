7b4567774e8b3254544a70b3f23207c920ee4e62
Victoria <vicsanfer04@gmail.com>
2023-10-22 20:12:06 +0100

    clase Plate

--- src/org/ulpgc/is1/model/Plate.java
+++ src/org/ulpgc/is1/model/Plate.java
@@ -1,6 +1,36 @@
 package org.ulpgc.is1.model;
 
 public class Plate {
+    private String number;
 
-    // victoria
+    public Plate(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public boolean isValid() {
+        if (number == null || number.isEmpty()) {
+            return false;
+        }
+
+        for (char character : number.toCharArray()) {
+            if (!Character.isLetterOrDigit(character)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
+}
+
+
+
+
